d7c05152459391528ff77717067c265039654e29
LizaM <liza.makarichak2019@gmail.com>
2023-07-12 20:50:49 +0300

    goit-js-hw-09

---
+++ src/js/02-timer.js
@@ -0,0 +1,79 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+const startBtnElem = document.querySelector('button[data-start]');
+const inputElem = document.querySelector('input[id="datetime-picker"]');
+const daysElem = document.querySelector('span[data-days]');
+const hoursElem = document.querySelector('span[data-hours]');
+const minutesElem = document.querySelector('span[data-minutes]');
+const secondsElem = document.querySelector('span[data-seconds]');
+const date = {
+    minDate: new Date(),
+};
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      console.log(selectedDates[0]);
+      if (selectedDates[0] <= date.minDate) {
+            Notiflix.Notify.failure("Please choose a date in the future");
+            startBtnElem.setAttribute('disabled', true);
+    } else {
+        startBtnElem.removeAttribute('disabled');
+    }
+  },
+};
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
+const datePicker = flatpickr(inputElem, options);
+const timerHandler = (evt) => {
+  const startTime = datePicker.selectedDates[0].getTime();
+  let intervalId = setInterval(() => {
+    const currentTime = Date.now();
+    const deltaTime = startTime - currentTime;
+    // повертає окремо days, hours, minutes, seconds
+    const convertData = convertMs(deltaTime);
+    updateInterface(convertData);
+    console.log(convertData);
+    if (deltaTime < 1000) {
+      clearInterval(intervalId);
+      return;
+    }
+  }, 1000);
+};
+  function updateInterface({ days, hours, minutes, seconds }) {
+  daysElem.textContent = addLeadingZero(days);
+  hoursElem.textContent = addLeadingZero(hours);
+  minutesElem.textContent = addLeadingZero(minutes);
+  secondsElem.textContent = addLeadingZero(seconds);
+}
+ const addLeadingZero = (value) => {
+      const UIvalue = String(value).padStart(2, '0');
+      return UIvalue;
+    };
+startBtnElem.addEventListener('click', timerHandler);
+
+
+
+
+
+
+
+
+
---
+++ src/js/03-promises.js
@@ -0,0 +1,48 @@
+import Notiflix from 'notiflix';
+const submitElem = document.querySelector('.form');
+const delayElem = document.querySelector('input[name="delay"]');
+const stepElem = document.querySelector('input[name="step"]');
+const amountElem = document.querySelector('input[name="amount"]');
+const onSubmitHandler = (evt) => {
+  evt.preventDefault();
+  let numberOfPromise = 0;
+  let amountDate = Number(amountElem.value);
+  let delayDate = Number(delayElem.value);
+  let stepDate = Number(stepElem.value);
+    if (delayDate<0||stepDate<0||amountDate<0) {
+    return
+    }
+  for (let i = 0; i < amountDate; i++) {
+    numberOfPromise += 1;
+    delayDate += stepDate;
+       createPromise(numberOfPromise, delayDate)
+      .then(({ numberOfPromise, delayDate }) => {
+         Notiflix.Notify.success(` Fulfilled promise ${numberOfPromise} in ${delayDate}ms`);
+      })
+      .catch(({ numberOfPromise, delayDate }) => {
+        Notiflix.Notify.failure(` Rejected promise ${numberOfPromise} in ${delayDate}ms`);
+      });
+  };
+  function createPromise(numberOfPromise, delayDate) {
+    return new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+      setTimeout(() => {
+        if (shouldResolve) {
+         resolve({numberOfPromise, delayDate})
+      } else {
+        reject({numberOfPromise, delayDate})
+      }
+      }, delayDate)
+    })
+  }
+}
+submitElem.addEventListener('submit', onSubmitHandler);
+
+
+
+
+
+
+
+
+
